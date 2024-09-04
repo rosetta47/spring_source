2191c42157e8eb1263703e934ecd424debe14424
hyunjung <ariestail@kakao.com>
2024-09-04 13:03:37 +0900

    websocket join chat leave

---
+++ sprweb32socket_chat/src/main/java/pack/Sprweb32socketChatApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb32socketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb32socketChatApplication.class, args);
+	}
+
+}
