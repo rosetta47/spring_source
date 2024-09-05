a4ba2e244ec1a8f88152ada38689139ca8d5fa02
hyunjung <ariestail@kakao.com>
2024-09-05 11:05:50 +0900

    socket alert

---
+++ sprweb33socket_alert/src/main/java/pack/Sprweb33socketAlertApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb33socketAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb33socketAlertApplication.class, args);
+	}
+
+}
